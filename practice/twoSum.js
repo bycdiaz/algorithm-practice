9ce751ec810c10cda9a9e01dd01878da68090cf4
Carlos Diaz <cdiaz86@gmail.com>
2021-01-21 15:07:03 -0800

    Adds solution to Two Sum exercise.

---
+++ practice/twoSum.js
@@ -0,0 +1,27 @@
+// Given an array of integers nums and an integer target,
+// return indices of the two numbers such that they add
+// up to target.
+
+// You may assume that each input would have exactly one
+// solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+function twoSum (nums, target) {
+  const set = new Set();
+
+  return nums.reduce((indices, currentNumber, index, array) => {
+    const currentDifference = target - currentNumber;
+
+    if (set.has(currentNumber)) {
+      indices = [index, array.indexOf(currentDifference)];
+    } else {
+      set.add(currentDifference);
+    }
+
+    return indices;
+  }, []);
+};
+
+console.log(twoSum([2,7,11,15], 9));
+// [ 1, 0 ]
